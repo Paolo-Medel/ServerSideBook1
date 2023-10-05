9b3290088092187f03cb2bb0a11ff1c9cc73356d
Paolo Medel <paolomedel17@gmail.com>
2023-10-05 14:56:28 -0400

    shift added to objects

--- index.py
+++ index.py
@@ -5,3 +5,6 @@ from swiming import Dolphin, Orca, Penguin, Seal, Whale
 from walking import Buffalo, Llama, Squirrel, Tiger, Wolf
 # import the python datetime module to help us create a timestamp
 from datetime import date
+
+Buffy = Buffalo("buffy", "great-plains", date.today(), "morning")
+print(f"{Buffy.name} will have the {Buffy.shift} shift")
--- walking/squirrel.py
+++ walking/squirrel.py
@@ -2,10 +2,11 @@ class Squirrel:
     """Creating a class for Squirrel
     """
 
-    def __init__(self, name, species, date_added):
+    def __init__(self, name, species, date_added, shift):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date_added
         self.walking = True
+        self.shift = shift
